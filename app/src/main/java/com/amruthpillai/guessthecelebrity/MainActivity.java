5b473f3f1e46367ce96e6941f9a51f41a40aae13
Amruth Pillai <im.amruth@gmail.com>
2017-07-24 14:56:41 +0530

    created DownloadTask

--- app/src/main/java/com/amruthpillai/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/amruthpillai/guessthecelebrity/MainActivity.java
@@ -1,13 +1,64 @@
 package com.amruthpillai.guessthecelebrity;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result= "";
+            URL url;
+            HttpURLConnection httpURLConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int data = inputStreamReader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                return null;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask downloadTask = new DownloadTask();
+        String result = "";
+
+        try {
+            result = downloadTask.execute("http://www.posh24.se/kandisar").get();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
