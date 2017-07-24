d2c1333d12cf6af121a562f6c92fccbb146b38e6
Amruth Pillai <im.amruth@gmail.com>
2017-07-24 15:59:33 +0530

    image downloading, displayed on the screen

--- app/src/main/java/com/amruthpillai/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/amruthpillai/guessthecelebrity/MainActivity.java
@@ -1,16 +1,57 @@
 package com.amruthpillai.guessthecelebrity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button button0, button1, button2, button3;
+
+    ArrayList<String> celebrityURLs = new ArrayList<>();
+    ArrayList<String> celebrityNames = new ArrayList<>();
+
+    int chosenCeleb = 0;
+    int locationOfCorrectAnswer = 0;
+    int[] answers = new int[4];
+
+    private class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.connect();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+                return bitmap;
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                return null;
+            }
+        }
+    }
 
     private class DownloadTask extends AsyncTask<String, Void, String> {
 
@@ -23,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 url = new URL(urls[0]);
                 httpURLConnection = (HttpURLConnection) url.openConnection();
+
                 InputStream inputStream = httpURLConnection.getInputStream();
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 
@@ -49,11 +91,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
         DownloadTask downloadTask = new DownloadTask();
-        String result = "";
+        String result = null;
 
         try {
             result = downloadTask.execute("http://www.posh24.se/kandisar").get();
+
+            String[] splitResult = result.split("<div class=\"sidebarContainer\">");
+
+            Pattern pattern = Pattern.compile("src=\"(.*?)\"");
+            Matcher matcher = pattern.matcher(splitResult[0]);
+
+            while (matcher.find()) {
+                celebrityURLs.add(matcher.group(1));
+            }
+
+            pattern = Pattern.compile("alt=\"(.*?)\"");
+            matcher = pattern.matcher(splitResult[0]);
+
+            while (matcher.find()) {
+                celebrityNames.add(matcher.group(1));
+            }
+
+            Random random = new Random();
+            chosenCeleb = random.nextInt(celebrityURLs.size());
+
+            ImageDownloader imageDownloader = new ImageDownloader();
+            Bitmap celebrityBitmap = imageDownloader.execute(celebrityURLs.get(chosenCeleb)).get();
+
+            imageView.setImageBitmap(celebrityBitmap);
+
+
 
         } catch (InterruptedException e) {
             e.printStackTrace();
